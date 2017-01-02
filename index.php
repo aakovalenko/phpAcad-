65ef59ce7011fadce3580414b6638238c7c5b5e6
kaaSLDN <kaa89pl@gmail.com>
2017-01-02 13:32:39 +0200

    Home Task 2 Kovalenko A.A.

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	 <link href='style.css' rel='stylesheet' type='text/css'>
+	<title>Резюме Коваленко А.А.</title>
+</head>
+<body>
+
+<h1>Коваленко Андрiй Андрiйович</h1>
+<div>
+<div class="leftBlock">
+<b>Дата народження:</b> 04.01.1989, Полтава</br>
+<b>Національність</b>: українець</br>	
+<b>Телефон:</b> +380663728562</br>
+<b>Ел.адреса:</b> kaa89pl@gmail.com</br>
+<b>Skype:</b> akaSaladin89</br>
+<b>Сімейний стан:</b> неодружений</br>
+</div>
+
+<div class="rightBlock">
+	<img src="img/SAM_4257.jpg" alt="myFoto" title="I">
+</div>
+</div>
+<div>
+<h2>Мета: <u>Вивчити мову программування php</u></h2>
+</div>
+
+<h3>Освiта:</h3>
+<p>2006-2010 Київський політехнічний інститут
+факультет “Телекомунікації”</p>
+<p>2010-2011 Київський політехнічний інститут
+факультет “Засоби військового зв’язку” спеціальність “Інженер електроніки” </p>
+
+<h3>Досвiд роботи: </h3>
+<p>2011-2012 ТОВ “Студія Місто” Інженер засобів телебачення</p>
+<p>2013-2014 ПП “Днепр-Сервис” Сервicний iнженер </p>
+<p>2014-2015 ВАТ “ПТМЗ” Системный администратор</p>
+<h3>Військова служба: </h3>
+<p>2012-2013 в/ч А1479 посада: Оператор відділення автоматизації.</p>
+<p>2015-2016 в/ч В03334 посада: Стрiлець</p>
+
+<h3>Особисті якості: </h3>
+	<div>
+		<table border="2" cellspacing='2'>
+			<tr>
+				<td>Знання мов</td>
+				<td>українська - рідна, російська - вільно, англійська – технічна</td>
+				
+			</tr>
+			<tr>
+				<td>Знання мов программування</td>
+				<td>HTML, CSS, базові знання JS, PHP</td>
+			</tr>
+		
+</table>
+<footer>
+	<ul>
+		<li ><a target="_blank" href="https://www.facebook.com"><img src="img/f.png"></a></li>
+		<li ><a target="_blank" href="https://vk.com"><img src="img/vk.png"></a></li>
+		<li ><a target="_blank" href="https://https://ok.ru/"><img src="img/odn.png"></a></li>
+		<li ><a target="_blank" href="https://https://google.com.ua/"><img src="img/g.png"></a></li>
+		<li ><a target="_blank" href="https://https://www.skype.com/"><img src="img/s.png"></a></li>
+	</ul>
+</footer>
+</body>
+</html>
